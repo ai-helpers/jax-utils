b206b53eff94e73097b8d5ab8baa98582df0efe1
data-corentinv <corentin.vasseur@decathlon.com>
2024-05-30 16:54:12 +0200

    fix: sphinx doc (remove tests directory)

--- tasks/documenters.mk
+++ tasks/documenters.mk
@@ -1,5 +1,5 @@
 document-api: ## Document the project API.
-	poetry run sphinx-apidoc -o docs/source/_api .
+	poetry run sphinx-apidoc -o docs/source/_api jax_utils
 
 document-%: ## Document the project in format %.
 	poetry run sphinx-build -b $* docs/source docs/build
