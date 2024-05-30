4737accc997a9b52829e468103c8b2e1eeccd455
data-corentinv <corentin.vasseur@decathlon.com>
2024-05-30 12:01:19 +0000

    Deploying to gh-pages from @ ai-helpers/jax-utils@033648993afc1f5b0284c70c2e21a6f75d292411 🚀

--- _static/conf.py
+++ _static/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, AI-Helpers"
 author = "AI-Helpers Team"
 
 # The full version, including alpha/beta/rc tags
-version = "0.1.0a0"
+version = "0.1.0a1"
 release = version
 
 # -- General configuration ---------------------------------------------------
